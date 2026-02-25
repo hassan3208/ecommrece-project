b4139918dcfa459448eb1fbe5ae7c84d2ed49e7c
Hassan <hassanimran1226@gmail.com>
2026-02-25 16:03:23 +0500

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import { AppHeader } from './AppHeader';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AppHeader />
+    </>
+  )
+}
+
+export default App
---
+++ src/AppHeader.jsx
@@ -0,0 +1,12 @@
+export function AppHeader() {
+  return (
+    <nav className="app-header">
+        
+        <img className="logo" src="https://supersimple.dev/projects/ecommerce/images/logo-white.svg" alt="logo" />
+        
+        <div></div>
+        <div></div>
+        <div></div>
+    </nav>
+  );
+}
