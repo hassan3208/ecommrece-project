b2ec47152a52f3a6d7c64447971f405c4f3f75b5
Hassan <hassanimran1226@gmail.com>
2026-02-27 14:38:15 +0500

    add routes to website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 // import { useState } from 'react'
-import { AppHeader } from './AppHeader';
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { orderPage as OrderPage } from './orderPage';
+import { TrackingPage } from './TrackingPage';
+import { CheckOut } from './CheckOut';
+
 
 function App() {
 
   return (
-    <>
-      <AppHeader />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/orders" element={<OrderPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+      <Route path="/checkout" element={<CheckOut />} />
+    </Routes>
   )
 }
 
